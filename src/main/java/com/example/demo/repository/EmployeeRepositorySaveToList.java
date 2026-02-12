76317f568cc2fec114f5f218ef929169a8620418
Aidar Ormokoev <gumasroid@gmail.com>
2026-02-12 15:16:27 +0600

    Добавлен список и маппер с конструктором

--- src/main/java/com/example/demo/repository/EmployeeRepositorySaveToList.java
+++ src/main/java/com/example/demo/repository/EmployeeRepositorySaveToList.java
@@ -1,14 +1,26 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Employee;
+import com.example.demo.mapper.EmployeeMapper;
 import com.example.demo.payload.request.EmployeeRequest;
 import com.example.demo.payload.response.EmployeeResponse;
 import com.example.demo.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service("list")
 public class EmployeeRepositorySaveToList implements EmployeeService {
+
+    private final List<Employee> employeeList = new ArrayList<>();
+    private final EmployeeMapper employeeMapper;
+
+    public EmployeeRepositorySaveToList(EmployeeMapper employeeMapper) {
+        this.employeeMapper = employeeMapper;
+    }
+
+
     @Override
     public EmployeeResponse save(EmployeeRequest employeeRequest) {
         return null;
