eba17a62af7fda6bd167da3d2750ec42a6cea66d
орозбай <altynbekov.bai@gmail.com>
2026-02-12 21:23:09 +0600

    refactor: refactor add cors

--- src/main/java/com/example/demo/controller/api/EmployeeController.java
+++ src/main/java/com/example/demo/controller/api/EmployeeController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import java.util.Map;
  * a save method (e.g., to a database, file, etc.).
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/employees")
 public final class EmployeeController {
     /**
